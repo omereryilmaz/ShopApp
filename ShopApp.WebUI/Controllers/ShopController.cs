75fb31b15cb2b30d8500e30f336f538d7cfe0c10
omereryilmaz <omreryilmaz@gmail.com>
2020-03-22 21:35:04 +0300

    Product list eklendi.

--- ShopApp.WebUI/Controllers/ShopController.cs
+++ ShopApp.WebUI/Controllers/ShopController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ShopApp.Business.Abstract;
+using ShopApp.WebUI.Models;
 
 namespace ShopApp.WebUI.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+
+        private IProductService _productService;
+
+        public ShopController(IProductService productService)
         {
-            return View();
+            _productService = productService;
+        }           
+
+        public IActionResult List()
+        {
+            return View(new ProductListModel()
+            {
+                Products = _productService.GetAll().ToList()
+            });
         }
+
     }
 }
