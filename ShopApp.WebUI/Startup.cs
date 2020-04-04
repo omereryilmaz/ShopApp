b48a581645bca51266339c6ece5fdf1626ec4866
omereryilmaz <omreryilmaz@gmail.com>
2020-04-04 19:20:21 +0300

    Identity eklendi.

--- ShopApp.WebUI/Startup.cs
+++ ShopApp.WebUI/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,23 +14,31 @@ using ShopApp.Business.Abstract;
 using ShopApp.Business.Concrete;
 using ShopApp.DataAccess.Abstract;
 using ShopApp.DataAccess.Concrete.EfCore;
+using ShopApp.WebUI.Identity;
 using ShopApp.WebUI.Middlewares;
 
 namespace ShopApp.WebUI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationIdentityDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("IdentityCOnnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddControllersWithViews();          
 
             services.AddScoped<IProductDal, EfCoreProductDal>();
@@ -56,6 +66,8 @@ namespace ShopApp.WebUI
 
             // extra klasorleri disari acmak icin
             app.CustomStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseRouting();
 
